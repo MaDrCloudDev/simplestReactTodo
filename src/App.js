5ba89dae20f10fed552e46147f0c0575684f2028
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-09-10 15:12:00 -0700

    init after finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+
+	const handleAddTodo = () => {
+		if (inputValue.trim() !== '') {
+			setTodos([...todos, { text: inputValue, completed: false }]);
+			setInputValue('');
+		}
+	};
+
+	const handleToggleTodo = (index) => {
+		const updatedTodos = [...todos];
+		updatedTodos[index].completed = !updatedTodos[index].completed;
+		setTodos(updatedTodos);
+	};
+
+	const handleDeleteTodo = (index) => {
+		const updatedTodos = todos.filter((_, i) => i !== index);
+		setTodos(updatedTodos);
+	};
+
+	return (
+		<div className='container'>
+			<h1>simplestJsTodo</h1>
+			<div className='input-wrapper'>
+				<input
+					type='text'
+					placeholder='Add a new todo'
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+				/>
+				<button
+					onClick={handleAddTodo}
+					className='add-todo-button'>
+					Add
+				</button>
+			</div>
+			<ul>
+				{todos.map((todo, index) => (
+					<li
+						key={index}
+						className={todo.completed ? 'completed' : ''}>
+						<input
+							type='checkbox'
+							checked={todo.completed}
+							onChange={() => handleToggleTodo(index)}
+						/>
+						<span>{todo.text}</span>
+						<button
+							onClick={() => handleDeleteTodo(index)}
+							className='delete-todo-button'>
+							Delete
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
